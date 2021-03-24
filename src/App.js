00b9a047e868746cfe9aa520d2ebb2b7f44f91b5
Sondinh <dnson20022001@gmail.com>
2021-03-24 22:44:05 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -1,52 +1,82 @@
 import React from "react";
 
 export default class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() }; // khai object state chua date = new Date()
-  }
-  // setup lifecycle
+    constructor(props) {
+        super(props);
+        this.state = { date: new Date() }; // khai object state chua date = new Date()
+    }
+    // setup lifecycle
 
-  // chay sau khi component da duoc render ra DOM
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
+    // chay sau khi component da duoc render ra DOM
+    componentDidMount() {
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
 
-  componentWillMount() {
-    clearInterval(this.timerID);
-  }
+    componentWillMount() {
+        clearInterval(this.timerID);
+    }
 
-  tick() {
-    this.setState({ date: new Date() }); // su dung setState de update component
-  }
+    tick() {
+        this.setState({ date: new Date() }); // su dung setState de update component
+    }
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
 }
 
 export class Toggle extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isToggleOn: true };
+    constructor(props) {
+        super(props);
+        this.state = { isToggleOn: true };
 
-    this.handleClick = this.handleClick.bind(this);
-  }
+        this.handleClick = this.handleClick.bind(this);
+        // can phai bind this vs this.handleClick trc khi pass sang onClick neu ko se bi undefined
+        console.log(this);
+    }
 
-  handleClick() {
-    this.setState((state) => ({ isToggleOn: !state.isToggleOn }));
+    handleClick() {
+        this.setState((state) => ({ isToggleOn: !state.isToggleOn }));
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick}>
+                {this.state.isToggleOn ? "ON" : "OFF"}
+            </button>
+        );
+    }
+    //* Cach khac ko dung bind
+    // Class field syntax
+    /*  
+    handleClick = () => {    
+      console.log('this is:', this);  
+    }
+    render() {
+      return (
+        <button onClick={this.handleClick}>
+          Click me
+        </button>
+      );
+    } */
+
+    // arrow function
+    /*
+      handleClick() {
+    console.log('this is:', this);
   }
 
   render() {
     return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? "ON" : "OFF"}
+      <button onClick={() => this.handleClick()}>        
+        Click me
       </button>
     );
   }
+    */
 }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Clock, { Toggle } from "./App";
 
-ReactDOM.render(<Clock></Clock>, document.getElementById("root"));
-ReactDOM.render(<Toggle></Toggle>, document.getElementById("root"));
+ReactDOM.render(
+    <>
+        <Clock></Clock>
+        <Toggle></Toggle>
+    </>,
+    document.getElementById("root")
+);
