b68c8130bd6645b2274d3318fa24d289db972c1f
Son Dinh <dnson20022001@gmail.com>
2021-04-03 16:50:52 +0700

    commit code

--- src/index.js
+++ src/index.js
@@ -2,33 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Clock, {
-    Toggle,
-    ListExample,
-    Blog,
-    NameForm,
-    Reservation,
-    Calculator,
+  Toggle,
+  ListExample,
+  Blog,
+  NameForm,
+  Reservation,
+  Calculator,
+  WelcomeDialog,
+  App,
 } from "./App";
 
 const numbers = [1, 2, 3, 4, 5, 6];
 const posts = [
-    { id: 1, title: "Hello World", content: "Welcome to learning React!" },
-    {
-        id: 2,
-        title: "Installation",
-        content: "You can install React from npm.",
-    },
+  { id: 1, title: "Hello World", content: "Welcome to learning React!" },
+  {
+    id: 2,
+    title: "Installation",
+    content: "You can install React from npm.",
+  },
 ];
 ReactDOM.render(
-    <>
-        <Clock></Clock>
-        <Toggle></Toggle>
-        <ListExample numbers={numbers}></ListExample>
-        <Blog posts={posts}></Blog>
-        <NameForm></NameForm>
-        <Reservation />
-        <br />
-        <Calculator />
-    </>,
-    document.getElementById("root")
+  <>
+    <Clock></Clock>
+    <Toggle></Toggle>
+    <ListExample numbers={numbers}></ListExample>
+    <Blog posts={posts}></Blog>
+    <NameForm></NameForm>
+    <Reservation />
+    <br />
+    <Calculator />
+    <br />
+    <WelcomeDialog />
+    <br />
+    <App></App>
+  </>,
+  document.getElementById("root")
 );
