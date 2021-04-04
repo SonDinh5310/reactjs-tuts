772b361dbcff06a65edb4200b1ed2251a6e24c01
Sondinh <dnson20022001@gmail.com>
2021-04-04 23:23:19 +0700

    push code

--- src/index.js
+++ src/index.js
@@ -2,39 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Clock, {
-  Toggle,
-  ListExample,
-  Blog,
-  NameForm,
-  Reservation,
-  Calculator,
-  WelcomeDialog,
-  App,
+    Toggle,
+    ListExample,
+    Blog,
+    NameForm,
+    Reservation,
+    Calculator,
+    WelcomeDialog,
+    App,
+    SignUpDialog,
 } from "./App";
 
 const numbers = [1, 2, 3, 4, 5, 6];
 const posts = [
-  { id: 1, title: "Hello World", content: "Welcome to learning React!" },
-  {
-    id: 2,
-    title: "Installation",
-    content: "You can install React from npm.",
-  },
+    { id: 1, title: "Hello World", content: "Welcome to learning React!" },
+    {
+        id: 2,
+        title: "Installation",
+        content: "You can install React from npm.",
+    },
 ];
 ReactDOM.render(
-  <>
-    <Clock></Clock>
-    <Toggle></Toggle>
-    <ListExample numbers={numbers}></ListExample>
-    <Blog posts={posts}></Blog>
-    <NameForm></NameForm>
-    <Reservation />
-    <br />
-    <Calculator />
-    <br />
-    <WelcomeDialog />
-    <br />
-    <App></App>
-  </>,
-  document.getElementById("root")
+    <>
+        <Clock></Clock>
+        <Toggle></Toggle>
+        <ListExample numbers={numbers}></ListExample>
+        <Blog posts={posts}></Blog>
+        <NameForm></NameForm>
+        <Reservation />
+        <br />
+        <Calculator />
+        <br />
+        <WelcomeDialog />
+        <br />
+        <App></App>
+        <br />
+        <SignUpDialog></SignUpDialog>
+    </>,
+    document.getElementById("root")
 );
